d2e0cb852c65bbd8c060ab572a2d5443c237a14c
Rohan Richards <scenes@quartermoondesigns.com>
2022-08-11 18:40:21 +1000

    acid pool weapon first pass

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -73,7 +73,7 @@ public class GameManager : MonoBehaviour
             }
         }
         yield return new WaitForSeconds(0.5f);
-        ItemBase startingItem = (ItemBase)playerScripts.GetComponent<Discharger>();
+        ItemBase startingItem = (ItemBase)playerScripts.GetComponent<AcidPool>();
         startingItem.IncreaseLevel();
         startingItem.Unlock();
         //StartCoroutine(SetWinGameTimer());
